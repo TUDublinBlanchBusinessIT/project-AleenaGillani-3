4badf53e32544e90f4698a12a8bbaa0d2d45f0c1
Aleena Gillani <B00152372@mytudublin.ie>
2025-04-18 13:34:36 +0100

    installed laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\MovieController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-// Resource routes for Genres and Movies
-Route::resource('genres', GenreController::class);
-Route::resource('movies', MovieController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
