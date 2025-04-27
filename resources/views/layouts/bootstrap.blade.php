63296f594cad0f3f963800ba87848f070fad14e0
Aleena Gillani <B00152372@mytudublin.ie>
2025-04-27 14:59:25 +0100

    polished the website look

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -4,45 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Collection</title>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 </head>
-<body>
+
+<body style="background: linear-gradient(135deg, #1a1a2e, #162447); color: #f5f5f5;">
+
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Movie Collection</a>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0f3460;">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <span style="font-size: 1.8rem; margin-right: 8px;">🎬</span>
+            Movie Collection
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('movies.index') }}">Movies</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('genres.index') }}">Genres</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('reviews.index') }}">Reviews</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('actors.index') }}">Actors</a>
-                </li>
-
+                <li class="nav-item"><a class="nav-link" href="{{ route('movies.index') }}">Movies</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('genres.index') }}">Genres</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('reviews.index') }}">Reviews</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('actors.index') }}">Actors</a></li>
             </ul>
 
             <ul class="navbar-nav">
                 @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                 @else
                     <li class="nav-item">
                         <span class="navbar-text mr-2">Welcome, {{ Auth::user()->name }}</span>
@@ -66,7 +62,6 @@
             </button>
         </div>
     @endif
-
 
     <!-- Page Content -->
     <div class="container mt-4">
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,9 @@ Route::middleware('auth')->group(function () {
     // ✅ Restore Routes
     Route::post('/movies/restore/{id}', [MovieController::class, 'restore'])->name('movies.restore');
     Route::post('/actors/restore/{id}', [ActorController::class, 'restore'])->name('actors.restore');
+    Route::post('/genres/restore/{id}', [GenreController::class, 'restore'])->name('genres.restore');
 
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
